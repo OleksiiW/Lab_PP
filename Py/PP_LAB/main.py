0d723738a2147e65bd6904a2e2f7f0b4719c0069
Oleksii <oleksii.wise@gmail.com>
2022-09-23 23:46:04 +0200

    Add wsgi - "Hello, World!"

--- Py/PP_LAB/main.py
+++ Py/PP_LAB/main.py
@@ -1,6 +1,12 @@
-def main():
-    pass
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
 
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=False, host='0.0.0.0')
