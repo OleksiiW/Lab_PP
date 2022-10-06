d480505daa1ec462e6f62e306e26cb6ad39245a7
Oleksii <oleksii.wise@gmail.com>
2022-10-06 23:34:22 +0200

    Add first app

--- Py/PP_LAB/app.py
+++ Py/PP_LAB/app.py
@@ -8,5 +8,10 @@ def hello_world():
     return "Hello, World! 24"
 
 
+@app.route('/about')
+def about():
+    return "About page"
+
+
 if __name__ == "__main__":
     app.run(debug=False, host='0.0.0.0')
